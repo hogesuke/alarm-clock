9802c6ba5b59e67812e6988e92a772ec6ad17c4b
hogesuke <miyado@gmail.com>
2016-12-04 02:53:42 +0900

    アラーム仮実装

---
+++ lib/alarm-clock.py
@@ -0,0 +1,13 @@
+from datetime import time
+from subprocess import Popen
+from time import sleep
+
+class AlarmClock:
+    def __init__(self):
+        self.alarm_time = time(5, 0, 0, 0)
+
+    def alarm(self):
+        cmd = '../bin/alarm'
+        proc = Popen(cmd.strip().split(' '))
+        sleep(5)
+        proc.terminate()
