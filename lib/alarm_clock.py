3d80d15e85efec658b95b477055a4606ffdb4a3e
hogesuke <miyado@gmail.com>
2016-12-04 17:17:15 +0900

    プロセスの終了判定を修正

--- lib/alarm_clock.py
+++ lib/alarm_clock.py
@@ -8,12 +8,11 @@ class AlarmClock:
         self.alarm_time = time(5, 0, 0, 0)
 
     def alarm(self):
-        cmd = "aplay -D hw:1,0 %s" % os.path.dirname(__file__) + '/../assets/sounds/alarm.wav'
+        cmd = 'aplay -D hw:1,0 %s' % os.path.dirname(__file__) + '/../assets/sounds/alarm.wav'
         proc = Popen(cmd.strip().split(' '))
 
         while True:
-            if proc.poll():
+            if proc.poll() == 0:
                 self.alarm()
                 break
             sleep(0.5)
-
