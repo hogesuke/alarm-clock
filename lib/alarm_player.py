f3ec1f4fae1657f26279800cb05e6cfbe7bd47d7
hogesuke <miyado@gmail.com>
2016-12-04 22:15:49 +0900

    アラームの再生をAlarmClockから直接Playerのインスタンスを生成して再生するように修正 その3

--- lib/alarm_player.py
+++ lib/alarm_player.py
@@ -5,18 +5,18 @@ from time import sleep
 class AlarmPlayer:
     def __init__(self):
         self.proc = None
-        self.is_stoped = False
+        self.is_stopped = False
 
     def play(self):
         cmd = 'aplay -D hw:1,0 %s' % os.path.dirname(__file__) + '/../assets/sounds/alarm.wav'
         self.proc = Popen(cmd.strip().split(' '))
 
         while True:
-            if self.proc.poll() == 0 or self.is_stoped:
+            if self.proc.poll() == 0 or not self.is_stopped:
                 self.play()
                 break
             sleep(0.2)
 
     def terminate(self):
-        self.is_stoped = True
+        self.is_stopped = True
         self.proc.terminate()
