0c4d3b6ba759ebc3904e906f9c9d1e44f366f750
hogesuke <miyado@gmail.com>
2016-12-04 17:41:06 +0900

    アラームを鳴らすクラスのクラス名を変更

--- lib/alarm_player.py
+++ lib/alarm_player.py
@@ -3,16 +3,16 @@ from datetime import time
 from subprocess import Popen
 from time import sleep
 
-class AlarmClock:
+class AlarmPlayer:
     def __init__(self):
         self.alarm_time = time(5, 0, 0, 0)
 
-    def alarm(self):
+    def play(self):
         cmd = 'aplay -D hw:1,0 %s' % os.path.dirname(__file__) + '/../assets/sounds/alarm.wav'
         proc = Popen(cmd.strip().split(' '))
 
         while True:
             if proc.poll() == 0:
-                self.alarm()
+                self.play()
                 break
-            sleep(0.5)
+            sleep(0.2)
