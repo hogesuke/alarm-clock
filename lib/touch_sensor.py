69fb7979ab3a1f6ed5d1ded1a8daab6d9d7587f1
hogesuke <miyado@gmail.com>
2016-12-06 20:49:50 +0900

    pin番号とpinModeに渡す値の誤り修正

--- lib/touch_sensor.py
+++ lib/touch_sensor.py
@@ -3,12 +3,10 @@ import wiringpi
 class TouchSensor:
 
     def __init__(self):
-        self.pin = 36  # GPIO16
+        self.pin = 16
 
         wiringpi.wiringPiSetupGpio()
-        wiringpi.pinMode(self.pin, 2)
+        wiringpi.pinMode(self.pin, 0)
 
     def is_touched(self):
         return wiringpi.digitalRead(self.pin) == 1
-
-
