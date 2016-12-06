dc59fabbfd6cd3c1e49a0798b30f5abb1b892c31
hogesuke <miyado@gmail.com>
2016-12-06 22:44:27 +0900

    #2 タッチセンサーに触れている間、アラームが止まるように修正

--- lib/alarm_clock.py
+++ lib/alarm_clock.py
@@ -2,44 +2,64 @@ import threading
 from datetime import datetime, time, timedelta
 from time import sleep
 from alarm_player import AlarmPlayer
+from touch_sensor import TouchSensor
 
 class AlarmClock:
     def __init__(self, hour=5, minute=0, second=0):
         self.alarm_time = time(hour, minute, second, 0)
-        self.wakeuped = self.alarm_time < datetime.now().time()
+        self.is_invoked = self.alarm_time < datetime.now().time()
+        self.is_plaing = False
+        self.is_pausing = False
         self.day = datetime.today().day
-        self.player = None
+        self.start_datetime = None
+        self.player = AlarmPlayer()
+        self.sensor = TouchSensor()
 
         self.run()
 
     def run(self):
         while True:
-            if self.__is_reached_wakeup_time():
-                start_datetime = datetime.now()
-
-                th = threading.Thread(target=self.sound, name='sound_thread')
-                th.setDaemon(True)
-                th.start()
-
-                self.wakeuped = True
-
-                while True:
-                    if self.__is_time_out(start_datetime):
-                        self.player.terminate()
-                        break
-                    sleep(1)
-
-            if self.__is_changed_day():
-                self.wakeuped = False
-
             sleep(1)
 
-    def sound(self):
-        self.player = AlarmPlayer()
+            if self.is_plaing:
+                sleep(0.2)
+
+                if self.is_pausing and not self.sensor.is_touched():
+                    self.is_pausing = False
+                    self.__sound()
+                    continue
+
+                if not self.is_pausing and self.sensor.is_touched():
+                    self.is_pausing = True
+                    self.player.terminate()
+                    continue
+
+                if self.__is_time_out(self.start_datetime):
+                    self.is_plaing = False
+                    self.player.terminate()
+                    continue
+
+            if self.__is_wakeup_time():
+                self.__sound()
+
+            if self.__is_changed_day():
+                self.is_invoked = False
+
+    def __sound(self):
+        self.start_datetime = datetime.now()
+
+        th = threading.Thread(target=self.__play, name='sound_thread')
+        th.setDaemon(True)
+        th.start()
+
+        self.is_invoked = True
+        self.is_plaing = True
+
+    def __play(self):
         self.player.play()
 
-    def __is_reached_wakeup_time(self):
-        return self.alarm_time <= datetime.now().time() and not self.wakeuped
+    def __is_wakeup_time(self):
+        return self.alarm_time <= datetime.now().time() and not self.is_invoked
 
     def __is_time_out(self, start_datetime):
         return (start_datetime + timedelta(minutes=10)).time() <= datetime.now().time()
