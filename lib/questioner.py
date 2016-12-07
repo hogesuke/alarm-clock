534f9bd992a54d90a8e49ebd439036e9f06e9d6e
hogesuke <miyado@gmail.com>
2016-12-08 05:06:56 +0900

    LCD表示のお試し実装を追加

---
+++ lib/questioner.py
@@ -0,0 +1,16 @@
+import webiopi
+
+from liquidcrystal import LiquidCrystal
+
+lcd = LiquidCrystal(7, 8, 25, 24, 23, 18)
+
+lcd.clear()
+lcd.setCursor(0, 0)
+lcd.write("Hello World!")
+
+counter = 1
+while 1:
+    lcd.set_cursor(0, 1)
+    lcd.write(str(counter))
+    counter += 1
+    webiopi.sleep(1.0)
